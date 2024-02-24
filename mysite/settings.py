3359cd7c63d7cbb470a1943ae5d8177a93dac4d7
ayinpoka <dorisakayili90@gmail.com>
2024-02-24 15:25:02 +0000

    add new host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wg2u3^d56#!grtlz8p2hyshx29rx(ryfyscm^e$tpz4in4bb)x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
